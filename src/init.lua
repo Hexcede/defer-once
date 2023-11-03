fadf06c0ab11e3cbd0fec4bf95d40dde038184d0
Hexcede <8394472+Hexcede@users.noreply.github.com>
2023-11-03 18:13:41 -0400

    Initial commit

---
+++ src/init.lua
@@ -0,0 +1,31 @@
+--!strict
+
+--- Takes in a callback and returns a new closure which will defer & call the callback exactly one time per defer period. Subsequent calls overwrite the arguments used.
+local function DeferOnce<A...>(callback: (A...) -> ...any): (A...) -> ()
+	local isDeferred = false
+	local args: { any } & { n: number } = {} :: any
+
+	return function(...)
+		-- Update with the most recent arguments
+		args = table.pack(...)
+
+		-- If the callback is already deferred, cancel
+		if isDeferred then
+			return
+		end
+
+		-- Mark that the callback is deferred
+		isDeferred = true
+
+		-- Defer & call the callback
+		task.defer(function()
+			-- Mark that the callback is no longer deferred
+			isDeferred = false
+
+			-- Call the callback with the most recent arguments
+			callback(table.unpack(args, 1, args.n))
+		end)
+	end
+end
+
+return DeferOnce
